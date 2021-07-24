375211f975ecf95e42c550b9b46ed1cb95c239e6
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-24 11:32:24 +0530

    Core: Update app

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,26 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo/amber-build-icon.png";
+import "./App.css";
 
 // COMPONENTS
 import Button from "./components/Button/index";
+import Header from "./components/Header/index";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <nav>
+          <img height={100} src={logo} alt="Logo for App" />
+        </nav>
+        <Header>Documentation For AmberBuild</Header>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "inherit",
+          }}
         >
-          Learn React
-        </a>
-      <Button onClick={() => console.log(document)} text="Hello" />
+          <Button text="Read the Docs" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Try AmberBuild
+          </a>
+        </div>
       </header>
     </div>
   );
