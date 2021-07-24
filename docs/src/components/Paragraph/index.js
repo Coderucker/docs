0acbd2373d18351dc05e0e9e07668a0a171ac4d4
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-24 12:49:12 +0530

    Component: Add Paragraph Component

---
+++ docs/src/components/Paragraph/index.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import './Header.css';
+
+/**
+ * Paragraph Component for AmberBuild Docs
+ */
+class Paragraph extends Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+    render() {
+        return (
+            <p className="paragraph__docs">
+                {this.props.text}
+                {this.props.children}
+            </p>
+        );
+    }
+}
+
+export default Paragraph;
