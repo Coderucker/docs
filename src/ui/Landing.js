f5fd6f9feb319904f440416a81b80ec696e5400f
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-25 22:09:27 +0530

    `Design`: Add main page

--- src/ui/Landing.js
+++ src/ui/Landing.js
@@ -9,24 +9,38 @@ const Landing = () => {
             display: "flex",
             flexDirection: "column",
             paddingTop: 150,
-            width: "50%",
+            width: "45%",
             paddingLeft: 40
         }}>
             <Header className="header__docs no-margin" style={{
-                fontSize: 50,
-                fontWeight: 900
+                fontSize: "200%",
+                fontWeight: 900,
+                textShadow: "2px 10px 35px #c4c4c4aa"
             }}>
                 Tired of Building and Executing Your Code
             </Header>   
             <Paragraph style={{
-                color: "#a3a3a3"
+                color: "#555",
+                fontSize: "75%"
             }}>
-                AmberBuild Automatically detects File Changes in your code<br/> and
+                AmberBuild Automatically detects File Changes in your code and
                 build according to the task you specified.
             </Paragraph>
-            <div>
-                <Button>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                maxWidth: "250px"
+            }}>
+                <Button style={{
+                    fontWeight: 600,
+                    boxShadow: "2px 5px 15px 0 #166edc60"
+                }}>
                     Read the Docs
+                </Button>
+                <Button isSec={true}>
+                    Try now
                 </Button>
             </div>
         </div>
--- src/ui/_Nav.js
+++ src/ui/_Nav.js
@@ -1,31 +1,51 @@
+import { useState } from "react";
 import Header from "../components/Header/index";
 import Nav from "../components/Nav";
 import NavLink from "../components/NavLink/index";
 import logo from "../assets/logo/amber-build-icon.png";
 
+// STYLES
+import '../styles/_Nav.css';
+
 /**
  * Nav Bar UI Implemented Component
  */
 const _Nav = () => {
+    const [scroll, setScroll] = useState(false);
+
+    window.onscroll = (e) => {
+      if (window.pageYOffset >= 20) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+
+      console.log(window.pageYOffset);
+    };
+
     return (
     <Nav style={{
         position: "fixed",
-        top: 0
+        top: 0,
+        background: scroll ? "rgba(255, 255, 255, 0.7)" : "#fff"
       }} animate="true">
         <div style={{
           display: "flex",
-          width: "20%",
+          width: "15%",
           height: "100%",
-          alignItems: "center",
-          justifyContent: "space-around"
+          alignItems: "center"
         }}>
           <img 
-            height={50} 
+            height={40} 
             src={logo} 
-            alt="Logo for App" />
+            alt="Logo for App"
+            style={{
+              margin: "0 10px",
+              marginBottom: "5px"
+            }} />
           <Header text="AmberBuild"/>
         </div>
-        <div style={{
+        <div className="supportive-links__docs" style={{
           display: "flex",
           width: "30%",
           height: "100%",
