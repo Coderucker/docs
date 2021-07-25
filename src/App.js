5a02557ef0b32695316348e40141bf961f14a009
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-25 22:12:49 +0530

    `Build`: Remove unused vars

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import "./App.css";
 import NavBarUI from "./ui/_Nav";
 import Landing from "./ui/Landing";
 
-// COMPONENTS
-import Nav from "./components/Nav/index";
-import Header from "./components/Header/index";
-import Paragraph from "./components/Paragraph/index";
-import Button from "./components/Button/index";
-import NavLink from "./components/NavLink/index";
-
-// UTILS
-import copyToClipBoard from "./util/copyToClipBoard";
-
 function App() {
   return (
     <div className="App">
--- src/ui/Landing.js
+++ src/ui/Landing.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Button from "../components/Button";
 import Header from "../components/Header";
 import Paragraph from "../components/Paragraph";
