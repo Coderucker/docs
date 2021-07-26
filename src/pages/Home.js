b8300fa03223b097d41888d971491d4af2ba34ac
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-26 22:34:34 +0530

    `Pages`: Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// UI
+import NavBarUI from "../ui/_Nav";
+import Landing from "../ui/Landing";
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavBarUI />
+            <Landing />
+        </div>
+    );
+}
+
+export default HomePage;
