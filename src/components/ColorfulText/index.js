0ec88b49a05fb284a2051ac6825f920dd0bf7b7d
Haneen Mahdin <72091386+haneenmahd@users.noreply.github.com>
2021-07-27 21:29:24 +0530

    Update index.js

--- src/components/ColorfulText/index.js
+++ src/components/ColorfulText/index.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 
 const ColorfulTextStyle = styled.span`
     background-image: linear-gradient(45deg, #F758FA, #72c, #456);
+    -webkit-background-clip: text;
     background-clip: text;
+    color: transparent;
 `;
 
 class ColorfulText extends React.Component {
